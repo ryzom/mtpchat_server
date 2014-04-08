1d8d625e77f112735453b1cc679850b2517c490a
kervala <none@none>
2014-04-08 13:43:19 +0200

    Changed: Compile dirent_win32.c only under Windows

--- src/dirent_win32.c
+++ src/dirent_win32.c
@@ -8,6 +8,8 @@
 
 */
 
+#ifdef _WIN32
+
 #include "dirent_win32.h"
 #include <errno.h>
 #include <io.h> /* _findfirst and _findnext set errno iff they return -1 */
@@ -146,3 +148,5 @@ void rewinddir(DIR *dir)
     But that said, if there are any problems please get in touch.
 
 */
+
+#endif
